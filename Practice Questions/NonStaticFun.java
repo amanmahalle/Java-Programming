63ccc8eb0dc0eef5e91a431cf188f6d9bfbe9619
amanmahalle <149795865+amanmahalle@users.noreply.github.com>
2026-01-06 20:47:02 +0530

    Delete Practice Questions directory

--- Practice Questions/NonStaticFun.java
+++
@@ -1,9 +0,0 @@
-public class NonStaticFun {
-    void printMessage(){
-        System.out.println("This a non-static method");
-    }
-    public static void main(String[] args) {
-        NonStaticFun obj = new NonStaticFun();
-        obj.printMessage();
-    }
-}
